584bdda1e58c638eee329da2c46a99e8e47e9bd6
antonz123 <aikidoanton@gmail.com>
2025-11-25 21:00:55 +0300

    Update 6.py

--- 6.py
+++ 6.py
@@ -1,18 +1,25 @@
 import csv
 
 
-def find_popular(books_en):
-    books_en.seek(0)
-    reader = csv.DictReader(books_en, delimiter=';')
+def find_most_popular_book(books_file):
+    """Find and print the most popular book based on download count."""
+    books_file.seek(0)
+    reader = csv.DictReader(books_file, delimiter=';')
     books = {}
 
     for book in reader:
         books[book['Book-Title']] = int(book['Downloads'])
 
-    res = max(books, key=lambda k: books[k])
-    print(f'Задание 6:\nСамая популярная книга: {res}\nКоличество скачиваний: {books[res]}')
+    most_popular_title = max(books, key=lambda k: books[k])
+    download_count = books[most_popular_title]
+    
+    print(
+        'Задание 6:\n'
+        f'Самая популярная книга: {most_popular_title}\n'
+        f'Количество скачиваний: {download_count}'
+    )
 
 
 if __name__ == '__main__':
-    with open('books-en.csv') as books_en:
-        find_popular(books_en)
+    with open('books-en.csv', encoding='utf-8') as books_file:
+        find_most_popular_book(books_file)
